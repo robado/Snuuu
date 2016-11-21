902c66222c2497f03cb8327eae28e4d7ce074866
Robert Ridal <robert2rid@yahoo.com>
2016-11-21 13:46:13 +0200

    Add files via upload

---
+++ Snöw+javasi/js/javasi.js
@@ -0,0 +1,24 @@
+
+
+
+
+var d = new Date();
+var pv = d.getDate():
+var kk = d.getMonth()+1;
+
+/*var paivat = document.getElementById("id").id;*/
+
+
+
+var x = paivat;
+if (kk == 11) { 
+   while (x >= 1) {
+       if (x <= 24) {
+           
+       }
+            
+   } else {
+       x--;
+   }
+       
+}
