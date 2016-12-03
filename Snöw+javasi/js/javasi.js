0cd2b06f2fed3f9cef6e58684515ff0368819aa9
Robert Ridal <robert2rid@yahoo.com>
2016-12-03 12:27:01 +0200

    Add files via upload

--- Snöw+javasi/js/javasi.js
+++ Snöw+javasi/js/javasi.js
@@ -1,14 +1,8 @@
-
-
-
-
-var d = new Date();
+/*var d = new Date();
 var pv = d.getDate():
 var kk = d.getMonth()+1;
 
-/*var paivat = document.getElementById("id").id;*/
-
-
+var paivat = document.getElementById("id").id;
 
 var x = paivat;
 if (kk == 11) { 
@@ -22,3 +16,84 @@ if (kk == 11) {
    }
        
 }
+
+/*function avaa(Luukku){
+    
+    if ( kk != 11){ 
+        alert("sus");
+    } else if (dd < dateLuukku) {
+        alert(";)");
+    } else {
+        document.getElementById(Luukku);
+    }
+}*/
+
+
+/*function tulostus(){
+    var x = document.getElementsByClassName("datetime");
+    
+    if(x == 1) {
+        alert("moi");
+    }
+}*/
+/*document.getElementById("nappi").disabled = true;
+
+function myFunction() {
+    document.getElementById("nappi").disabled = true;
+    var x = document.getElementById("id").id;
+    if (x == 1) {
+        window.alert("sus");
+    }
+}*/
+/*ovi = function () {
+    // Your code here
+
+
+    function ovi(ovet) {
+        var t = new Date();
+        var day = t.getDate();
+        var mm = t.getMonth() + 1;
+        if (ovet <= day && mm == 12) {
+            alert("sus");
+        } else {
+            alert("sas");
+
+        }
+    }
+    return false;
+}*/
+
+ovi = function (ovet) {
+        var t = new Date();
+        var day = t.getDate();
+        var mm = t.getMonth() + 1;
+        if (ovet <= day && mm == 12) {
+            alert("sus");
+            return true;
+        } else {
+            alert("sas");
+            return false;
+        }  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
